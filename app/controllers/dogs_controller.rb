b73e584334745afcba2200a343397cf5c8842357
alexrevell <revellalex@gmail.com>
2015-06-07 12:57:16 +1200

    index page view has dog edit link, register new dog link, forms prepared for edit dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+  def update
+    @dog = Dog.find(params[:dog])
+  end
+
+  def edit
+    render :edit
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resource :users
   resource :dogs
+  # edit_dog '/dogs/:id' to: 'dogs#update' as: update_dog
 end
