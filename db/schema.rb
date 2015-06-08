dcbcf5aee63c4c05c3583569749b58befc8a70b7
alexrevell <revellalex@gmail.com>
2015-06-08 16:45:41 +1200

    migration for registered status and registered_until date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606030014) do
+ActiveRecord::Schema.define(version: 20150608043415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20150606030014) do
     t.string   "breed"
     t.date     "date_of_birth"
     t.integer  "owner_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "registered",       default: false
+    t.date     "registered_until"
   end
 
   create_table "users", force: :cascade do |t|
