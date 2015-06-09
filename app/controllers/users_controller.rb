859c96fd69d87043ff6785ffe8f8c0cb74be0662
alexrevell <revellalex@gmail.com>
2015-06-09 16:10:50 +1200

    separate partial view from main index for user or admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
   def index
     @user = current_user
+    @users = User.all
+  end
+
+  def _admin_index
+    @users - User.all
   end
 
   def show
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,4 +4,9 @@ class VisitorsController < ApplicationController
     @user = current_user
   end
 
+  def index
+    @user = current_user
+    @users = User.all
+  end
+
 end
